d4ac9279cf71b4c9c171af2ae0bc22d3a5e4bd41
Mohit27mk <114071726+Mohit27mk@users.noreply.github.com>
2023-02-01 02:33:51 +0530

    Making server

--- first-app.js
+++ first-app.js
@@ -1,4 +1,32 @@
-const fs=require('fs');
-console.log("Hello world");
+// // const fs=require('fs');
+// // console.log("Hello world");
 
-fs.writeFileSync('hello.txt',"Hello from mohit");
+// // fs.writeFileSync('hello.txt',"Hello from mohit");
+
+// // const multiply=(a,b)=>a*b;
+
+// // console.log(multiply(3,5));
+
+
+// // const Student={
+// //   name:"Mohit",
+// //   age:20,
+// //   gender:"Male"
+// // };
+
+// // const fruits=['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
+
+// // console.log(fruits.map(fruit=>fruit==' '?'empty string':fruit));
+
+// const obj1 = {'key1': 1, "key2": 2, "key3": 1000}
+
+// let { key1, key3} = obj1
+
+
+
+// key1 = 20;
+
+// key3 = 123
+
+// console.log(obj1.key1, obj1.key3)
+
---
+++ server.js
@@ -0,0 +1,11 @@
+const http=require('http');
+
+// function rqListner(req,res){
+
+// }
+
+const server=http.createServer((req,res)=>{
+ console.log("Mohit");
+});
+
+server.listen(4000);
