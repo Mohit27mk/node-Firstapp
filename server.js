3fbcb06090a29f0c0dfc72bf18519c1e6c56bb35
Mohit27mk <114071726+Mohit27mk@users.noreply.github.com>
2023-02-01 19:28:51 +0530

    req-res

--- server.js
+++ server.js
@@ -5,7 +5,32 @@ const http=require('http');
 // }
 
 const server=http.createServer((req,res)=>{
- console.log("Mohit");
+
+    const url=req.url;
+    if(url==='/home'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<head><title>My first Page</title></head>');
+        res.write('<body><h1>Welcome Home!</h1></body>');
+        res.write('</html>');
+        res.end();
+    }else if(url==='/about'){
+        res.setHeader('Content-Type','text/html');
+ res.write('<html>');
+ res.write('<head><title>My first Page</title></head>');
+ res.write('<body><h1>Welcome to About Us page!</h1></body>');
+ res.write('</html>');
+ res.end();
+    }else if(url==='/node'){
+        res.setHeader('Content-Type','text/html');
+ res.write('<html>');
+ res.write('<head><title>My first Page</title></head>');
+ res.write('<body><h1>Welcome to my Node Js project!</h1></body>');
+ res.write('</html>');
+ res.end();
+    }
+
+ 
 });
 
 server.listen(4000);
