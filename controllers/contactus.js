fb0cf2d2a23e7f30c64327b0f094bdea75cb9099
Mohit27mk <114071726+Mohit27mk@users.noreply.github.com>
2023-02-04 23:57:25 +0530

    controllers

---
+++ controllers/contactus.js
@@ -0,0 +1,12 @@
+const rootDir=require('../util/path');
+const path=require('path');
+
+
+exports.getContact=(req,res,next)=>{
+    res.sendFile(path.join(rootDir,'views','contact.html'))
+};
+
+exports.postContact=(req,res,next)=>{
+    console.log(req.body); 
+   res.redirect('/success');
+ };
--- route/contact.js
+++ route/contact.js
@@ -1,17 +1,11 @@
 const express =require('express');
-const path=require('path');
 
 const router =express.Router();
 
-const rootDir=require('../util/path');
+const contactUs=require('../controllers/contactus')
 
-router.get('/contact',(req,res,next)=>{
-    res.sendFile(path.join(rootDir,'views','contact.html'))
-});
+router.get('/contact',contactUs.getContact);
 
-router.post('/contact',(req,res,next)=>{
-   console.log(req.body); 
-  res.redirect('/success');
-});
+router.post('/contact',contactUs.postContact);
 
 module.exports=router;
